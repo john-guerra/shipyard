ff010aeb301c94c0af3188b85774a08f8b15956b
jgmurillo10 <jg.murillo10@uniandes.edu.co>
2019-03-17 00:57:14 -0500

    Updates

---
+++ precache-manifest.1b8236c1916c7c8f739bb43f537f0878.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "445111add21889f2c89f",
+    "url": "./static/js/main.dda91c8d.chunk.js"
+  },
+  {
+    "revision": "4ebb8d6941999264b333",
+    "url": "./static/js/2.13685a06.chunk.js"
+  },
+  {
+    "revision": "445111add21889f2c89f",
+    "url": "./static/css/main.780e3f3e.chunk.css"
+  },
+  {
+    "revision": "4ebb8d6941999264b333",
+    "url": "./static/css/2.f4e55b08.chunk.css"
+  },
+  {
+    "revision": "518dd4b32dd74cb8ff27f01bf7f1b5a4",
+    "url": "./index.html"
+  }
+];
